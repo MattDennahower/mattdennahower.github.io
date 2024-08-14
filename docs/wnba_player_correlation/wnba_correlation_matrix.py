96819b3c318ecadae11077e1f7188d7b814973d2
Matt D <matthewdennahower@gmail.com>
2024-08-14 09:07:59 -0400

    update filtering of teams

--- docs/wnba_player_correlation/wnba_correlation_matrix.py
+++ docs/wnba_player_correlation/wnba_correlation_matrix.py
@@ -9,22 +9,24 @@ data = pd.read_csv('wnba_2024.csv')
 values = 'AST'  # Change this to the statistic you want
 
 # Define the team you're interested in
-team_to_plot = 'IND'  # Change this to the team you want
+team_to_plot = ''  # Change this to the team you want, leave empty to process all teams
+
+# Get the list of teams to process
+teams_to_process = data['team_id'].unique() if not team_to_plot else [team_to_plot]
 
 # For each unique team in the data
 for team in data['team_id'].unique():
-    if team == team_to_plot:
-        # Filter the data for the current team
-        team_data = data[data['team_id'] == team]
-        
-        # Pivot the filtered data so that each row is a game, each column is a player, and the values are assists
-        pivot_data = team_data.pivot(index='game_id', columns='Starters', values=values)
-        
-        # Compute the correlation matrix for assists
-        assists_corr = pivot_data.corr()
-        
-        # Create a heatmap of the correlation matrix
-        plt.figure(figsize=(10, 10))
-        sns.heatmap(assists_corr, cmap='coolwarm', center=0, annot=False)
-        plt.title(f'Correlation Matrix of {values} between Players for Team {team}')
-        plt.show()
+    # Filter the data for the current team
+    team_data = data[data['team_id'] == team]
+    
+    # Pivot the filtered data so that each row is a game, each column is a player, and the values are assists
+    pivot_data = team_data.pivot(index='game_id', columns='Starters', values=values)
+    
+    # Compute the correlation matrix for assists
+    assists_corr = pivot_data.corr()
+    
+    # Create a heatmap of the correlation matrix
+    plt.figure(figsize=(10, 10))
+    sns.heatmap(assists_corr, cmap='coolwarm', center=0, annot=False)
+    plt.title(f'Correlation Matrix of {values} between Players for Team {team}')
+    plt.show()
